5f805cbacd07849f12783f5772eba0c96d54eb41
Jovana <jovana1005@gmail.com>
2024-11-25 21:49:02 +0100

    fixed substr

--- ft_substr.c
+++ ft_substr.c
@@ -12,27 +12,43 @@
 
 #include "libft.h"
 
+static int	calculate_substring_size(size_t start_index, size_t src_length, size_t max_len)
+{
+	if (start_index < src_length)
+	{
+		if (max_len <= (src_length - start_index))
+		{
+			return (max_len);
+		}
+		return (src_length - start_index);
+	}
+	return (0);
+}
+
 char	*ft_substr(char const *s, unsigned int start, size_t len)
 {
 	char	*substr;
 	size_t	i;
+	size_t	j;
+	size_t	s_len;
+	size_t	substring_size;
 
-	i = 0;
-	substr = (char *) malloc (len * sizeof(char) + 1);
-	if (!substr || (start >= ft_strlen(s)))
-	{
+	s_len = ft_strlen(s);
+	substring_size = calculate_substring_size(start, s_len, len);
+	substr = (char *) malloc (substring_size * sizeof(char) + 1);
+	if (!substr)
 		return ((void *)0);
-	}
-	else
 	{
-		while (i < len)
-		{
-			substr[i] = s[start];
-			i++;
-			start++;
-		}
-		substr[i] = '\0';
-		return (substr);
+	i = 0;
+	j = start;
+	while ((j < s_len) && (j < (start + len)))
+	{
+		substr[i] = s[j];
+		i++;
+		j++;
+	}
+	substr[i] = '\0';
+	return (substr);
 	}
 }
 /*
