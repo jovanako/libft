4cc1eaa5872157d86a44c6802dbfca319e37c4ad
Jovana <jovana1005@gmail.com>
2024-11-27 20:54:40 +0100

    changed strlcat

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -18,8 +18,10 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	size_t	src_i;
 	size_t	total_len;
 
-	total_len = ft_strlen(dst) + ft_strlen(src);
+	total_len = ft_strlen(src) + ft_strlen(dst);
 	dst_i = ft_strlen(dst);
+	if ((size - dst_i) == 0)
+		return (total_len);
 	src_i = 0;
 	while (size > 0 && src[src_i] != '\0')
 	{
@@ -29,3 +31,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t size)
 	dst[dst_i] = '\0';
 	return (total_len);
 }
+/*
+#include <string.h>
+#include <stdio.h>
+
+int main()
+{
+	char dst[30];
+	memset(dst, 0, 30);
+	char *src = (char *)"AAAAAAAAA";
+	dst[0] = 'B';
+	printf("Total length: %zu\nsrc length: %zu\n",
+			ft_strlcat(dst, src, 1), ft_strlen(src));
+	printf("Dest: %s\n", dst);
+	return 0;	
+}*/
