a21f262e596b944543be4733a47d28ccb86862c2
Jovana <jovana1005@gmail.com>
2024-12-02 11:16:28 +0100

    edited split

--- ft_split.c
+++ ft_split.c
@@ -12,7 +12,7 @@
 
 #include "libft.h"
 
-static size_t	string_count(char *s, char c)
+static int	string_count(char *s, char c)
 {
 	size_t	i;
 	size_t	s_len;
@@ -29,17 +29,17 @@ static size_t	string_count(char *s, char c)
 		}
 		if ((s[i] == c) && (s[s_len - 1] == c)) 
 		{
-			count--;
+			count -= 2;
 		}
-		while (s[i] != '\0')
+		while (i != s_len)
 		{
 			if (s[i] == c)
 			{
 				count++;
-				i++;
 			}
 			while (s[i] == c)
-				i++;		
+				i++;
+			i++;		
 		}
 	}
 	return (count);
@@ -52,39 +52,45 @@ char	**ft_split(char const *s, char c)
 	char	**result;
 	char	*start;
 	char	*next;
-	size_t	s_end;
 	size_t	i;
+	size_t	s_len;
 	int		parts_count;
-	int		*delimiters;
 
 	str = (char *)s;
-	s_end = ft_strlen(str) - 1;
-	parts_count = string_count(str, c);
-	if (parts_count == 0)
-		parts_count = 1;
-	result = (char **)malloc((parts_count + 1) * sizeof(char *));
-	if (!result)
-		return (0);
-	if (!ft_strchr(str, c))
-		result[0] = str;
-	next = str;
-	i = 0;
-	while (*str)
+	s_len = ft_strlen(s);
+	if (*s)
 	{
-		start = ft_strchr(str, c);
-		if ((start != &str[s_end])
-			&& (next != &str[s_end]))
-		{
-			next = ft_strchr(start + 1, c);
-		}
-		split = (char *)malloc((next - start + 1) * sizeof(char));
-		if (!split)
+		parts_count = string_count(str, c);
+		if (parts_count == 0)
+			parts_count = 1;
+		result = (char **)malloc((parts_count + 1) * sizeof(char *));
+		if (!result)
 			return (0);
-		split = ft_substr(str, start - str, next - start);
-		split[next - start] = '\0';
-		result[i] = split;
-		i++;
-		str = (char *)&s_end;
+		if (!ft_strchr(str, c))
+			result[0] = str;
+		next = str;
+		while (str != &(s[s_len]))
+		{
+			if (*str == c)
+			{
+				while (*str == c)
+				str++;
+			}
+			start = str;
+			i = 0;
+			if (start != &(s[s_len]))
+			{
+				if (ft_strchr(start, c))
+					next = ft_strchr(start, c);
+				else
+					next = str + s_len;
+				split = ft_substr(s, start - s, next - start);
+				result[i] = split;
+				i++;
+				str = next;
+			}
+		}
 	}
-	result[parts_count] = (void *)0; 
+	result[parts_count] = (void *)0;
+	return (result);
 }
